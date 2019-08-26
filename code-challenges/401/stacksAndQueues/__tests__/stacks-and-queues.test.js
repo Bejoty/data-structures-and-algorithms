784051d14ab7d782037d9266bd59d753103b74b2
Austin Hedeen <austin.hedeen@gmail.com>
2019-08-25 22:47:44 -0700

    Outline test cases

---
+++ code-challenges/401/stacksAndQueues/__tests__/stacks-and-queues.test.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const {Stack, Queue} = require('../stacks-and-queues');
+
+let stack, queue;
+
+
+beforeEach(() => {
+  stack = new Stack();
+  queue = new Queue();
+});
+
+
+describe('Testing Stack', () => {
+
+  it('can successfully push onto a stack', () => {
+
+  });
+
+  it('can successfully push multiple values onto a stack', () => {
+
+  });
+
+  it('can successfully pop off the stack', () => {
+
+  });
+
+  it('can successfully empty a stack after multiple pops', () => {
+
+  });
+
+  it('can successfully peek the next item on the stack', () => {
+
+  });
+
+  it('can successfully instantiate an empty stack', () => {
+
+  });
+
+});
+
+
+describe('Testing Queue', () => {
+
+  it('can successfully enqueue into a queue', () => {
+
+  });
+
+  it('can successfully enqueue multiple values into a queue', () => {
+
+  });
+
+  it('can successfully dequeue out of a queue the expected value', () => {
+
+  });
+
+  it('can successfully peek into a queue, seeing the expected value', () => {
+
+  });
+
+  it('can successfully empty a queue after multiple dequeues', () => {
+
+  });
+
+  it('can successfully instantiate an empty queue', () => {
+
+  });
+
+});
