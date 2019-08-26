edf4ce4174603278db51223cef41ae13760adc0b
Austin Hedeen <austin.hedeen@gmail.com>
2019-08-26 09:11:04 -0700

    Add documentation

--- code-challenges/401/stacksAndQueues/stacks-and-queues.js
+++ code-challenges/401/stacksAndQueues/stacks-and-queues.js
@@ -3,22 +3,37 @@
 const { Node, LinkedList } = require('../linkedList/linked-list');
 
 
-// TODO - Stack Documentation
+/**
+ * @class Stack
+ * @extends LinkedList
+ */
 class Stack extends LinkedList {
 
   constructor() {
     super();
   }
 
+  /**
+   * Returns true if stack is empty, else false
+   * @returns {boolean}
+   */
   isEmpty() {
     return !this.head;
   }
 
+  /**
+   * Pushes the given value onto the stack
+   * @param value
+   */
   push(value) {
     const node = new Node(value, this.head);
     this.head = node;
   }
 
+  /**
+   * Pops the top value off of the stack
+   * @returns {null|*} - the popped value, or null if stack is empty
+   */
   pop() {
     if (this.isEmpty()) {
       return null;
@@ -29,6 +44,10 @@ class Stack extends LinkedList {
     return value;
   }
 
+  /**
+   * Returns the top value of the stack, without removing it
+   * @returns {null|*} - the top value, or null if stack is empty
+   */
   peek() {
     if (this.isEmpty()) {
       return null;
@@ -40,9 +59,9 @@ class Stack extends LinkedList {
 }
 
 
-// TODO - Queue Documentation
 /**
  * @class Queue
+ * @extends LinkedList
  *
  * The specification asks for a `front` property, and a O(1) enqueue method.
  * For a singly linked list, a `front` reference would lead to a O(1) dequeue
@@ -56,10 +75,18 @@ class Queue extends LinkedList {
     super();
   }
 
+  /**
+   * Returns true if queue is empty, else false
+   * @returns {boolean}
+   */
   isEmpty() {
     return !this.head;
   }
 
+  /**
+   * Adds a value to the back of the queue
+   * @param value - the value to be added
+   */
   enqueue(value) {
     const node = new Node(value);
 
@@ -75,6 +102,10 @@ class Queue extends LinkedList {
     }
   }
 
+  /**
+   * Removes a value from the front of the queue
+   * @returns {null|*} - the removed value, or null if the queue is empty
+   */
   dequeue() {
     if (this.isEmpty()) {
       return null;
@@ -85,6 +116,10 @@ class Queue extends LinkedList {
     return value;
   }
 
+  /**
+   * Returns the value at the front of the list, without removing it
+   * @returns {null|*} - the value at the front of the queue, or null if the queue is empty
+   */
   peek() {
     if (this.isEmpty()) {
       return null;
